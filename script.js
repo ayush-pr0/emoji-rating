cd6a24a3dfd65ffc586fcabe0aa16f4fdb624f01
ayush-geeks <ayushkhandelwal0608@gmail.com>
2022-05-10 04:38:45 +0530

    Submit button added

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const starsEl = document.querySelectorAll(".fa-star");
 const emojisEl = document.querySelectorAll(".fa-regular");
 const colors = ["red", "orange", "lightblue", "lightgreen", "green"];
+const button = document.querySelector(".button");
+let rate = 1;
 
 const updateRating = function (index) {
+    rate = index + 1;
     starsEl.forEach((starEl, id) => {
         if (id < index + 1) starEl.classList.add("active");
         else starEl.classList.remove("active");
@@ -20,4 +23,10 @@ starsEl.forEach((starEl, index) => {
     starEl.addEventListener("click", () => {
         updateRating(index);
     });
+});
+
+button.addEventListener("click", () => {
+    console.log(`Rating : ${rate}`);
+    rate = 1;
+    updateRating(0);
 });
